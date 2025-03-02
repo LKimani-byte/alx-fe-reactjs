8cbdecbf9777e6a81d76ccef447cd2479c6b7ec6
leon kimani <l.kimani064@gmail.com>
2025-03-02 16:14:36 +0300

    second test

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-
+import counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +19,7 @@ function App() {
       <MainContent />
       <Footer />
       <WelcomeMessage />
+      <counter /> 
             <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
